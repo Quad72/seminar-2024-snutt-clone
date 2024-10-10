f99d797035d191aea677ed40badddebeaa8aa1d6
Quad72 <quad.heo@snu.ac.kr>
2024-10-10 22:16:36 +0900

    메인 로그인 화면 완성

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,31 @@ export const App = () => {
 
   return (
     <div className={styles.main}>
-      <img src={waffleLogo} />
+      <div className={styles.logobox}>
+        <img src={waffleLogo} className={styles.wafflelogo} />
+        <span className={styles.timetabletext}>TimeTable</span>
+      </div>
+      <div className={styles.toolbox}>
+        <div className={styles.loginbox}>
+          <button className={styles.loginbutton}>로그인</button>
+          <h5 className={styles.registertext}>회원가입</h5>
+        </div>
+        <div className={styles.snsbox}>
+          <div className={styles.snstextbox}>
+            <hr className={styles.snstextline}></hr>
+            <div className={styles.snstext}>SNS 계정으로 계속하기</div>
+            <hr className={styles.snstextline}></hr>
+          </div>
+          <div className={styles.snslogobox}>
+            <div className={styles.Kakaologo}></div>
+            <div className={styles.Googlelogobox}>
+              <div className={styles.Googlelogo}></div>
+            </div>
+            <div className={styles.Facebooklogo}></div>
+            <div className={styles.Applelogo}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
