f683a0b248a47196c191e65b8884091768a75559
Quad72 <quad.heo@snu.ac.kr>
2024-10-12 13:39:07 +0900

    vercel 이미지 재재수정

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import leftarrow from './assets/left-arrow.png';
 import styles from './css/LoginPage.module.css';
 
-const LoginPage = () => {
+type LoginProps = {
+  setToken: (token: string) => void;
+};
+
+type TokenResponse = {
+  user_id: string;
+  token: string;
+  message: string;
+};
+
+const LoginPage = ({ setToken }: LoginProps) => {
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate(-1);
+  };
+
+  const handleLoginClick = async () => {
+    try {
+      const response = await fetch(
+        'https://wafflestudio-seminar-2024-snutt-redirect.vercel.app/v1/auth/login_local',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id, password }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error('로그인 실패');
+      }
+
+      const data = (await response.json()) as TokenResponse;
+      setToken(data.token);
+      navigate('/profile');
+    } catch (error) {
+      console.error('error: ', error);
+    }
   };
 
   return (
@@ -26,6 +64,10 @@ const LoginPage = () => {
             className={styles.idinput}
             type="text"
             id="id"
+            value={id}
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
             placeholder="아이디를 입력하세요"
           ></input>
         </div>
@@ -35,6 +77,10 @@ const LoginPage = () => {
             className={styles.passwordinput}
             type="password"
             id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             placeholder="비밀번호를 입력하세요"
           ></input>
         </div>
@@ -45,7 +91,16 @@ const LoginPage = () => {
         </div>
       </div>
       <div className={styles.buttonbox}>
-        <button className={styles.loginbutton}>로그인</button>
+        <button
+          className={styles.loginbutton}
+          onClick={() => {
+            handleLoginClick().catch(() => {
+              console.error('error');
+            });
+          }}
+        >
+          로그인
+        </button>
       </div>
     </div>
   );
